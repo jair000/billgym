a0be41296ef1616fa4dc2b9c111e80108f89a95b
eorbezom <eorbezo99@gmail.com>
2025-05-25 20:42:03 -0500

    se sube los archivos loguin

---
+++ billgym/src/main/java/com/billgym/pe/repository/LoguinRepository.java
@@ -0,0 +1,15 @@
+package com.billgym.pe.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.billgym.pe.entity.Loguin;
+
+public interface LoguinRepository extends JpaRepository<Loguin, Integer> {
+	@Query("SELECT l FROM Loguin l JOIN l.usuarioDato u WHERE u.dni = :dni")
+	List<Loguin> buscarPorDniUsuario(@Param("dni")Integer usuario);
+
+}
---
+++ billgym/src/main/java/com/billgym/pe/service/LoguinService.java
@@ -0,0 +1,8 @@
+package com.billgym.pe.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoguinService {
+
+}
