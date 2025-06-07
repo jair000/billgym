c94053f73155930efb2a972e4ce7d932b655376c
Coco <102814124+jair000@users.noreply.github.com>
2025-06-07 15:06:16 -0500

    1 prueba de commit: Se elimina importacion que no se usa

--- billgym/src/main/java/com/billgym/pe/controller/LoguinController.java
+++ billgym/src/main/java/com/billgym/pe/controller/LoguinController.java
@@ -1,6 +1,5 @@
 package com.billgym.pe.controller;
 
-import java.net.ResponseCache;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -85,63 +84,58 @@ public class LoguinController {
 	// PARA LOGUEARSE EL USUARIO
 	@GetMapping("/login")
 	public String mostrarFormularioLoguin(Model model, HttpSession session) {
-	    if(session.getAttribute("usuarioLogueado") != null) {
-	        return "redirect:/home";
-	    }
-	    model.addAttribute("loguin", new Loguin()); 
-	    
-	    return "formularioLogin";
+		if (session.getAttribute("usuarioLogueado") != null) {
+			return "redirect:/home";
+		}
+		model.addAttribute("loguin", new Loguin());
+
+		return "formularioLogin";
 	}
 
 	// PARA VALIDAR Y GUARDAR SESION DE LOGUIN
-	//FALTA IMPLEMENTAR
+	// FALTA IMPLEMENTAR
 	@PostMapping("/login")
 	public String procesarLogin(@ModelAttribute("loguin") Loguin loguin,
-	                            HttpSession session,
-	                            RedirectAttributes redirectAttributes,
-	                            Model model) {
+			HttpSession session,
+			RedirectAttributes redirectAttributes,
+			Model model) {
 
-	    Loguin usuarioValido = loguinService.validarCredenciales(loguin.getUsuario(), loguin.getPassword());
+		Loguin usuarioValido = loguinService.validarCredenciales(loguin.getUsuario(), loguin.getPassword());
 
-	    if (usuarioValido != null) {
-	        session.setAttribute("usuarioLogueado", usuarioValido);
-	        return "redirect:/home";
-	    } else {
-	        model.addAttribute("error", "Usuario o contraseña incorrectos");
-	        model.addAttribute("loguin", loguin);
-	        return "formularioLogin"; 
-	    }
+		if (usuarioValido != null) {
+			session.setAttribute("usuarioLogueado", usuarioValido);
+			return "redirect:/home";
+		} else {
+			model.addAttribute("error", "Usuario o contraseña incorrectos");
+			model.addAttribute("loguin", loguin);
+			return "formularioLogin";
+		}
 	}
-	
-	
+
 	// MOSTRAR HOME SOLO SI ESTÁ LOGUEADO
 	@GetMapping("/home")
-	public String mostrarHome(Model model, HttpSession session, HttpServletResponse response ) {
+	public String mostrarHome(Model model, HttpSession session, HttpServletResponse response) {
 		if (session.getAttribute("usuarioLogueado") == null) {
 			return "redirect:/login";
 		}
-		//PARA QUE CUANDO EL USUARIO RETROCEDE NO ESTE  LOGUEADO EL PAGINA BORRA EL CACHE
+		// PARA QUE CUANDO EL USUARIO RETROCEDE NO ESTE LOGUEADO EL PAGINA BORRA EL
+		// CACHE
 		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
 		response.setHeader("Pragma", "no-cache");
 		response.setDateHeader("Expires", 0);
-		
-		
+
 		Loguin usuario = (Loguin) session.getAttribute("usuarioLogueado");
-		String nombreCompleto = (usuario.getUsuarioDato()!=null)
+		String nombreCompleto = (usuario.getUsuarioDato() != null)
 				? usuario.getUsuarioDato().getNombres()
-				:usuario.getUsuario();
-	    model.addAttribute("nombreUsuario", nombreCompleto);
-		return "home"; 
+				: usuario.getUsuario();
+		model.addAttribute("nombreUsuario", nombreCompleto);
+		return "home";
 	}
-	
+
 	@GetMapping("/logout")
 	public String cerrarSesion(HttpSession session) {
-	    session.invalidate();
-	    return "redirect:/login";
+		session.invalidate();
+		return "redirect:/login";
 	}
-
-
-	
-	
 
 }
